9401fd0b13f1bd4f1c3d7ff215540cd8ae67303d
Artur <info@aigroupusa.com>
2022-02-15 21:04:34 -0500

    commit tree 3ef0324a0f674a6f7eff67ac306a165369c90cbe

---
+++ src/Cell.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Cell extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            color: this.props.value
+        } // ...define initial state with a key of 'color' set to the 'value' prop
+      }
+
+      hanleClick = () => {
+          this.setState({
+              color: '#333'
+          })
+      }
+
+    render() {
+        return (
+            <div
+                className="cell"
+                style={{backgroundColor: this.state.color}}
+                onClick={this.hanleClick}> 
+            </div>
+        );
+    }
+}
+
+export default Cell;
